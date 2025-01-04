092abd65edcd93440e50627edbdb7c40cee29aae
Rusan Alexandru <rusannalexandru@gmail.com>
2025-01-04 18:10:46 +0200

    intial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Biblioteca_Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// 1. Add HttpClient service
+builder.Services.AddHttpClient();
+
+// 2. Add Razor Components
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+// 3. Map your Razor component endpoints
+app.MapRazorComponents<App>()
+   .AddInteractiveServerRenderMode();
+
+app.Run();
